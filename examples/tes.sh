ccca495b0a1bd328d897c2562c49423553abb212
yn-mac <yingnn@live.cn>
2018-08-06 11:03:45 +0800

    c tes.sh

--- examples/tes.sh
+++ examples/tes.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 function info() {
-    echo Usage: `basename $0` package_name
+    echo Usage: `basename $0` [vs] virtual_name config list
     exit 1
 }
 
-while getopts ":p:" opt
+while getopts ":pvs" opt
 do
     case $opt in
         p) out_prefix=$OPTARG;;
+        v) virtual=new;;
         *) info;;
     esac
 done
@@ -17,15 +18,18 @@ shift $(($OPTIND - 1))
 
 if [ $# -lt 1 ]; then info; fi
 
-. ~/.config/yfulrc
+package_name=git@gitee.com:yingnn/cal.git
 
+if test -n "$virtual"; then
+    virtual="pip install virtualenv && virtualenv $1"
+    package="pip install $package_name"
+fi
 
-echo $ref_genome
+test -n "$package" && package="pip install $package_name"
 
-echo ~
-
-echo test
-get_abs abc
-
-set_done
+eval $vritual
+unset PYTHONPATH
+. $1/bin/activate
+eval $package
+yful --config $2 -p$out_prefix $3
 
